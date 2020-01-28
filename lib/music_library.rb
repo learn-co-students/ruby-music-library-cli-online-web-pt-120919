2d21a287c6d26142fdc6d5e33fba8341846cdc02
Jade Lynn Masker <hopefulfire@gmail.com>
2020-01-28 10:00:14 -0500

    Done.

---
+++ lib/music_library.rb
@@ -0,0 +1,89 @@
+class Song
+
+	attr_accessor :name, :artist
+
+	@@all = []
+
+	def initialize(name, artist=nil)
+		@name = name
+		@artist = artist
+	end
+
+	def save
+		@@all << self
+	end
+
+	def self.create(name)
+		new_obj = new(name)
+		new_obj.save
+	end
+
+	def self.all
+		@@all
+	end
+
+	def self.destroy_all
+		@@all.clear
+	end
+
+end
+
+
+class Artist
+
+	attr_accessor :name
+
+	@@all = []
+
+	def initialize(name)
+		@name = name
+	end
+
+	def save
+		@@all << self
+	end
+
+	def self.create(name)
+		new_obj = new(name)
+		new_obj.save
+	end
+
+	def self.all
+		@@all
+	end
+
+	def self.destroy_all
+		@@all.clear
+	end
+
+end
+
+
+class Genre
+
+	attr_accessor :name
+
+	@@all = []
+
+	def initialize(name)
+		@name = name
+	end
+
+	def save
+		@@all << self
+	end
+
+	def self.create(name)
+		new_obj = new(name)
+		new_obj.save
+	end
+
+	def self.all
+		@@all
+	end
+
+	def self.destroy_all
+		@@all.clear
+	end
+
+end
